c50b67afbfa91ff4efa25fe229a481ab1fca8ea9
Slava Imeshev <imeshev@yahoo.com>
2015-07-20 19:06:35 -0700

    Test: Added a tester for InvalidParameterException

---
+++ test/src/org/cacheonix/exceptions/InvalidParameterExceptionTest.java
@@ -0,0 +1,50 @@
+package org.cacheonix.exceptions;
+
+import junit.framework.TestCase;
+
+/**
+ * A tester for {@link InvalidParameterException}.
+ */
+public final class InvalidParameterExceptionTest extends TestCase {
+
+
+   private InvalidParameterException invalidParameterException;
+
+   private Throwable cause;
+
+
+   /**
+    * Tests the the costructor correctly sets the cause.
+    */
+   public void testConstructor() {
+
+      assertSame(cause, invalidParameterException.getCause());
+   }
+
+
+   public void setUp() throws Exception {
+
+      super.setUp();
+
+      cause = new Throwable();
+      invalidParameterException = new InvalidParameterException(cause);
+   }
+
+
+   public void tearDown() throws Exception {
+
+      cause = null;
+      invalidParameterException = null;
+
+      super.tearDown();
+   }
+
+
+   public String toString() {
+
+      return "InvalidParameterExceptionTest{" +
+              "cause=" + cause +
+              ", invalidParameterException=" + invalidParameterException +
+              "} " + super.toString();
+   }
+}
