838be4901ea3d37b4d5cbfc30fd7e5ad051d860b
simeshev <simeshev@cacheonix.org>
2016-01-04 08:03:36 -0800

    Added CacheInvalidatorFactoryTest.

---
+++ test/src/org/cacheonix/impl/cache/invalidator/CacheInvalidatorFactoryTest.java
@@ -0,0 +1,64 @@
+package org.cacheonix.impl.cache.invalidator;
+
+import java.util.Properties;
+
+import junit.framework.TestCase;
+import org.cacheonix.cache.invalidator.CacheInvalidator;
+import org.cacheonix.cache.invalidator.CacheInvalidatorContext;
+
+/**
+ * A tester for {@link CacheInvalidatorFactory}.
+ */
+public final class CacheInvalidatorFactoryTest extends TestCase {
+
+
+   public static final String PROPERTY_VALUE = "test.property.value";
+
+   public static final String PROPERTY_NAME = "test.property";
+
+   public static final String CACHE_NAME = "test.cache";
+
+   private CacheInvalidatorFactory cacheInvalidatorFactory;
+
+
+   /**
+    * Tests {@link CacheInvalidatorFactory#createInvalidator(String, String, Properties)}
+    */
+   public void testCreateInvalidator() {
+
+      final Properties invalidatorProperties = new Properties();
+      invalidatorProperties.setProperty(PROPERTY_NAME, PROPERTY_VALUE);
+      final CacheInvalidator invalidator = cacheInvalidatorFactory.createInvalidator(CACHE_NAME,
+              TestCacheInvalidator.class.getName(), invalidatorProperties);
+
+      assertTrue(invalidator instanceof TestCacheInvalidator);
+      final TestCacheInvalidator testCacheInvalidator = (TestCacheInvalidator) invalidator;
+      final CacheInvalidatorContext context = testCacheInvalidator.getContext();
+      assertEquals(CACHE_NAME, context.getCacheName());
+      assertEquals(invalidatorProperties, context.getProperties());
+   }
+
+
+   public void setUp() throws Exception {
+
+      super.setUp();
+
+      cacheInvalidatorFactory = new CacheInvalidatorFactory();
+   }
+
+
+   public void tearDown() throws Exception {
+
+      cacheInvalidatorFactory = null;
+
+      super.tearDown();
+   }
+
+
+   public String toString() {
+
+      return "CacheInvalidatorFactoryTest{" +
+              "cacheInvalidatorFactory=" + cacheInvalidatorFactory +
+              '}';
+   }
+}
---
+++ test/src/org/cacheonix/impl/cache/invalidator/TestCacheInvalidator.java
@@ -0,0 +1,39 @@
+package org.cacheonix.impl.cache.invalidator;
+
+import java.util.Properties;
+
+import org.cacheonix.cache.invalidator.CacheInvalidator;
+import org.cacheonix.cache.invalidator.CacheInvalidatorContext;
+import org.cacheonix.cache.invalidator.Invalidateable;
+
+/**
+ * A test cache invalidator that allos to verify that ptoper methods are called when {@link
+ * CacheInvalidatorFactory#createInvalidator(String, String, Properties)} is called.
+ */
+public final class TestCacheInvalidator implements CacheInvalidator {
+
+   private CacheInvalidatorContext context;
+
+
+   public void setContext(final CacheInvalidatorContext context) {
+
+      this.context = context;
+   }
+
+
+   public void process(final Invalidateable cacheElement) {
+   }
+
+
+   CacheInvalidatorContext getContext() {
+
+      return context;
+   }
+
+   public String toString() {
+
+      return "TestCacheInvalidator{" +
+              "context=" + context +
+              '}';
+   }
+}
