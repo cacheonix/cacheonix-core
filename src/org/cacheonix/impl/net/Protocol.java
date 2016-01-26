15f82c43e30e8fd7a3fdd74a78add8e32842f3ea
simeshev <simeshev@cacheonix.org>
2016-01-26 12:45:08 -0800

    Bumped up protocol number to becuase of CACHEONIX-455 - Optimize in-memory footprint of null

--- src/org/cacheonix/impl/net/Protocol.java
+++ src/org/cacheonix/impl/net/Protocol.java
@@ -31,7 +31,7 @@ public final class Protocol {
    /**
     * Protocol version.
     */
-   public static final int PROTOCOL_VERSION = (byte) 8;
+   public static final int PROTOCOL_VERSION = (byte) 11;
 
 
    private Protocol() {
