e6d9342471ce3a6dddd96131751b2cd7b3e08411
simeshev <simeshev@cacheonix.org>
2016-01-25 17:40:46 -0800

    Code quality:  removed redundant check for null

--- src/org/cacheonix/util/NetworkInterfaceEnumerator.java
+++ src/org/cacheonix/util/NetworkInterfaceEnumerator.java
@@ -25,6 +25,8 @@ import java.util.List;
 import org.cacheonix.Version;
 import org.cacheonix.impl.util.StringUtils;
 
+import static org.cacheonix.impl.util.StringUtils.toYesNo;
+
 
 /**
  * This utility class outputs list of network interfaces to System.out.
@@ -92,38 +94,82 @@ public final class NetworkInterfaceEnumerator {
          stream.println(NAME + netIf.getName());
          stream.println(DISPLAY_NAME + netIf.getDisplayName());
          stream.println(INET_ADDRESS_ES + inetAddressesToString(netIf));
-         if (getInterfaceAddressesAvailable(netIf)) {
-            stream.println(INTERFACE_ADDRESS_ES + interfaceAddresses(netIf));
+
+         try {
+
+            final List interfaceAddresses = (List) getInterfaceAddressesMethod(netIf).invoke(netIf, EMPTY_OBJECT_ARRAY);
+            final StringBuilder buf = new StringBuilder(DEFAULT_STRING_BUFFER_LENGTH);
+            for (int i = 0, n = interfaceAddresses.size(); i < n; i++) { // NOPMD
+               final Object interfaceAddress = interfaceAddresses.get(i);
+               final Method getAddressMethod = interfaceAddress.getClass().getMethod("getAddress", EMPTY_CLASS_ARRAY);
+               final Method getNetworkPrefixLengthMethod = interfaceAddress.getClass().getMethod(
+                       "getNetworkPrefixLength",
+                       EMPTY_CLASS_ARRAY);
+               buf.append(StringUtils.toString(
+                       (InetAddress) getAddressMethod.invoke(interfaceAddress, EMPTY_OBJECT_ARRAY))).append('/')
+                       .append(getNetworkPrefixLengthMethod.invoke(interfaceAddress, EMPTY_OBJECT_ARRAY).toString());
+               if (i < n - 1) {
+                  buf.append(", ");
+               }
+            }
+            stream.println(INTERFACE_ADDRESS_ES + buf.toString());
+         } catch (final NoSuchMethodException ignored) {
+            // Multicast not supported
          }
-         if (mtuAvailable(netIf)) {
-            stream.println(MTU + getMTUasString(netIf));
+
+         try {
+
+            final Method mtuMethod = getMTUMethod(netIf);
+            stream.println(MTU + ((Number) mtuMethod.invoke(netIf, EMPTY_OBJECT_ARRAY)).intValue());
+         } catch (final NoSuchMethodException ignored) {
+            // Multicast not supported
          }
-         if (supportsMulticastAvailable(netIf)) {
-            stream.println(SUPPORTS_MULTICAST + StringUtils.toYesNo(supportsMulticast(netIf)));
+
+         try {
+
+            final Method supportsMulticastMethod = getSupportsMulticastMethod(netIf);
+            final Boolean supportsMulticast = (Boolean) supportsMulticastMethod.invoke(netIf, EMPTY_OBJECT_ARRAY);
+            stream.println(SUPPORTS_MULTICAST + toYesNo(supportsMulticast));
+         } catch (final NoSuchMethodException ignored) {
+            // Multicast not supported
          }
-         if (isUpAvailable(netIf)) {
-            stream.println(UP + StringUtils.toYesNo(isUp(netIf)));
+
+         try {
+
+            final Method isUpMethod = getIsUpMethod(netIf);
+            final Boolean isUp = (Boolean) isUpMethod.invoke(netIf, EMPTY_OBJECT_ARRAY);
+            stream.println(UP + toYesNo(isUp));
+         } catch (final NoSuchMethodException ignore) {
+            // Is up is not supported
          }
-         if (isLoopbackAvailable(netIf)) {
-            stream.println(LOOPBACK + StringUtils.toYesNo(isLoopback(netIf)));
+
+         try {
+            final Method isLoopbackMethod = getIsLoopbackMethod(netIf);
+            final Boolean isLoopback = (Boolean) isLoopbackMethod.invoke(netIf, EMPTY_OBJECT_ARRAY);
+            stream.println(LOOPBACK + toYesNo(isLoopback));
+         } catch (final NoSuchMethodException ignore) {
+            // isLoopback is not supported
          }
-         if (isPointToPointAvailable(netIf)) {
-            stream.println(POINT_TO_POINT + StringUtils.toYesNo(isPointToPoint(netIf)));
+
+         try {
+
+            final Method isPointToPointMethod = getIsPointToPointMethod(netIf);
+            final Boolean isPointToPoint = (Boolean) isPointToPointMethod.invoke(netIf, EMPTY_OBJECT_ARRAY);
+            stream.println(POINT_TO_POINT + toYesNo(isPointToPoint));
+         } catch (final NoSuchMethodException ignore) {
+            // isPointToPoint is not supported
          }
-         if (isVirtualAvailable(netIf)) {
-            stream.println(VIRTUAL + StringUtils.toYesNo(isVirtual(netIf)));
+
+         try {
+
+            final Method isVirtualMethod = getIsVirtualMethod(netIf);
+            final Object isVirtual = isVirtualMethod.invoke(netIf, EMPTY_OBJECT_ARRAY);
+            stream.println(VIRTUAL + toYesNo((Boolean) isVirtual));
+         } catch (final NoSuchMethodException ignore) {
+            // isPointToPoint is not supported
          }
+
          stream.println();
-      }
-   }
-
-
-   private boolean isVirtualAvailable(final NetworkInterface netIf) {
-
-      try {
-         return getIsVirtualMethod(netIf) != null;
-      } catch (final NoSuchMethodException e) {
-         return false;
       }
    }
 
@@ -134,44 +180,9 @@ public final class NetworkInterfaceEnumerator {
    }
 
 
-   private boolean isVirtual(final NetworkInterface netIf) throws IllegalAccessException,
-           InvocationTargetException, NoSuchMethodException {
-
-      return (Boolean) getIsVirtualMethod(netIf).invoke(netIf, EMPTY_OBJECT_ARRAY);
-   }
-
-
-   private boolean isPointToPointAvailable(final NetworkInterface netIf) {
-
-      try {
-         return getIsPointToPointMethod(netIf) != null;
-      } catch (final NoSuchMethodException e) {
-         return false;
-      }
-   }
-
-
-   private Method getIsPointToPointMethod(final NetworkInterface netIf)
-           throws NoSuchMethodException {
+   private Method getIsPointToPointMethod(final NetworkInterface netIf) throws NoSuchMethodException {
 
       return netIf.getClass().getMethod("isPointToPoint", EMPTY_CLASS_ARRAY);
-   }
-
-
-   private boolean isPointToPoint(final NetworkInterface netIf) throws IllegalAccessException,
-           InvocationTargetException, NoSuchMethodException {
-
-      return (Boolean) getIsPointToPointMethod(netIf).invoke(netIf, EMPTY_OBJECT_ARRAY);
-   }
-
-
-   private boolean isLoopbackAvailable(final NetworkInterface netIf) {
-
-      try {
-         return getIsLoopbackMethod(netIf) != null;
-      } catch (final NoSuchMethodException e) {
-         return false;
-      }
    }
 
 
@@ -181,67 +192,15 @@ public final class NetworkInterfaceEnumerator {
    }
 
 
-   private boolean isLoopback(final NetworkInterface netIf) throws IllegalAccessException,
-           InvocationTargetException, NoSuchMethodException {
-
-      return (Boolean) getIsLoopbackMethod(netIf).invoke(netIf, EMPTY_OBJECT_ARRAY);
-   }
-
-
-   private boolean isUpAvailable(final NetworkInterface netIf) {
-
-      try {
-         return getIsUpMethod(netIf) != null;
-      } catch (final NoSuchMethodException e) {
-         return false;
-      }
-   }
-
-
    private Method getIsUpMethod(final NetworkInterface netIf) throws NoSuchMethodException {
 
       return netIf.getClass().getMethod("isUp", EMPTY_CLASS_ARRAY);
    }
 
 
-   private boolean isUp(final NetworkInterface netIf) throws IllegalAccessException,
-           InvocationTargetException, NoSuchMethodException {
-
-      return (Boolean) getIsUpMethod(netIf).invoke(netIf, EMPTY_OBJECT_ARRAY);
-   }
-
-
-   private boolean supportsMulticastAvailable(final NetworkInterface netIf) {
-
-      try {
-         return getSupportsMulticastMethod(netIf) != null;
-      } catch (final NoSuchMethodException e) {
-         return false;
-      }
-   }
-
-
-   private Method getSupportsMulticastMethod(final NetworkInterface netIf)
-           throws NoSuchMethodException {
+   private Method getSupportsMulticastMethod(final NetworkInterface netIf) throws NoSuchMethodException {
 
       return netIf.getClass().getMethod("supportsMulticast", EMPTY_CLASS_ARRAY);
-   }
-
-
-   private boolean supportsMulticast(final NetworkInterface netIf) throws IllegalAccessException,
-           InvocationTargetException, NoSuchMethodException {
-
-      return (Boolean) getSupportsMulticastMethod(netIf).invoke(netIf, EMPTY_OBJECT_ARRAY);
-   }
-
-
-   private boolean mtuAvailable(final NetworkInterface netIf) {
-
-      try {
-         return getMTUMethod(netIf) != null;
-      } catch (final NoSuchMethodException e) {
-         return false;
-      }
    }
 
 
@@ -251,53 +210,10 @@ public final class NetworkInterfaceEnumerator {
    }
 
 
-   private int getMTUasString(final NetworkInterface netIf) throws IllegalAccessException,
-           InvocationTargetException, NoSuchMethodException {
-
-      return ((Number) getMTUMethod(netIf).invoke(netIf, EMPTY_OBJECT_ARRAY)).intValue();
-   }
-
-
-   private boolean getInterfaceAddressesAvailable(final NetworkInterface netIf) {
-
-      try {
-         return getInterfaceAddressesMethod(netIf) != null;
-      } catch (final NoSuchMethodException e) {
-         return false;
-      }
-   }
-
-
    private Method getInterfaceAddressesMethod(final NetworkInterface netIf)
            throws NoSuchMethodException {
 
       return netIf.getClass().getMethod("getInterfaceAddresses", EMPTY_CLASS_ARRAY);
-   }
-
-
-   /**
-    * Transform a List InterfaceAddresses provided by NetworkInterface to a string.
-    *
-    * @param netIf NetworkInterface providing InterfaceAddressees
-    * @return String
-    */
-   private String interfaceAddresses(final NetworkInterface netIf) throws NoSuchMethodException,
-           IllegalAccessException, InvocationTargetException {
-
-      final List interfaceAddresses = (List) getInterfaceAddressesMethod(netIf).invoke(netIf, EMPTY_OBJECT_ARRAY);
-      final StringBuilder buf = new StringBuilder(DEFAULT_STRING_BUFFER_LENGTH);
-      for (int i = 0, n = interfaceAddresses.size(); i < n; i++) { // NOPMD 
-         final Object interfaceAddress = interfaceAddresses.get(i);
-         final Method getAddressMethod = interfaceAddress.getClass().getMethod("getAddress", EMPTY_CLASS_ARRAY);
-         final Method getNetworkPrefixLengthMethod = interfaceAddress.getClass().getMethod("getNetworkPrefixLength", EMPTY_CLASS_ARRAY);
-         buf.append(StringUtils.toString((InetAddress) getAddressMethod.invoke(interfaceAddress, EMPTY_OBJECT_ARRAY))).append('/')
-                 .append(getNetworkPrefixLengthMethod.invoke(interfaceAddress, EMPTY_OBJECT_ARRAY).toString());
-         if (i < n - 1) {
-            buf.append(", ");
-         }
-
-      }
-      return buf.toString();
    }
 
 
@@ -320,10 +236,10 @@ public final class NetworkInterfaceEnumerator {
             firstLine = false;
          }
          buf.append("  Canonical host name: ").append(address.getCanonicalHostName()).append(LINE_SEPARATOR);
-         buf.append("   Site-local address: ").append(StringUtils.toYesNo(address.isSiteLocalAddress())).append(LINE_SEPARATOR);
-         buf.append("   Link local address: ").append(StringUtils.toYesNo(address.isLinkLocalAddress())).append(LINE_SEPARATOR);
-         buf.append("    Any local address: ").append(StringUtils.toYesNo(address.isAnyLocalAddress())).append(LINE_SEPARATOR);
-         buf.append("     Loopback address: ").append(StringUtils.toYesNo(address.isLoopbackAddress())).append(LINE_SEPARATOR);
+         buf.append("   Site-local address: ").append(toYesNo(address.isSiteLocalAddress())).append(LINE_SEPARATOR);
+         buf.append("   Link local address: ").append(toYesNo(address.isLinkLocalAddress())).append(LINE_SEPARATOR);
+         buf.append("    Any local address: ").append(toYesNo(address.isAnyLocalAddress())).append(LINE_SEPARATOR);
+         buf.append("     Loopback address: ").append(toYesNo(address.isLoopbackAddress())).append(LINE_SEPARATOR);
          buf.append("            Host name: ").append(address.getHostName()).append(LINE_SEPARATOR);
       }
       return buf.toString();
