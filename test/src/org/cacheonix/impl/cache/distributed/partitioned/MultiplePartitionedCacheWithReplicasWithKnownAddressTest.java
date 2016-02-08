2a4efa10a79468a9521275791e150cc29784c5d1
Slava Imeshev <imeshev@yahoo.com>
2016-02-07 19:37:25 -0800

    Refactoring: extracted superclass 'MultiplePartitionedCacheWithReplicasTestCase'

--- test/src/org/cacheonix/impl/cache/distributed/partitioned/MultiplePartitionedCacheWithReplicasWithKnownAddressTest.java
+++ test/src/org/cacheonix/impl/cache/distributed/partitioned/MultiplePartitionedCacheWithReplicasWithKnownAddressTest.java
@@ -13,13 +13,6 @@
  */
 package org.cacheonix.impl.cache.distributed.partitioned;
 
-import java.util.Map;
-import java.util.Set;
-
-import org.cacheonix.cache.Cache;
-import org.cacheonix.cache.CacheStatistics;
-import org.cacheonix.impl.util.array.HashMap;
-import org.cacheonix.impl.util.array.HashSet;
 import org.cacheonix.impl.util.logging.Logger;
 
 /**
@@ -28,7 +21,7 @@ import org.cacheonix.impl.util.logging.Logger;
  * @noinspection ProhibitedExceptionDeclared, ProhibitedExceptionDeclared, ConstantNamingConvention,
  * ConstantNamingConvention, ConstantNamingConvention, ConstantNamingConvention, ConstantNamingConvention
  */
-public final class MultiplePartitionedCacheWithReplicasWithKnownAddressTest extends MultiplePartitionedCacheTestCase {
+public final class MultiplePartitionedCacheWithReplicasWithKnownAddressTest extends MultiplePartitionedCacheWithReplicasTestCase {
 
    /**
     * Logger.
@@ -50,71 +43,6 @@ public final class MultiplePartitionedCacheWithReplicasWithKnownAddressTest exte
    public void testCoherenceGetAllClear() throws Exception { // NOPMD
 
       super.testCoherenceGetAllClear();
-   }
-
-
-   /**
-    * This method overwrites driver's becuase with GetRequest and GetAllRequest reading from the local buckets and
-    * repartitioning between replicas in process statistics behaves unpredictably. This method removes volitile
-    * asserts.
-    */
-   public void testGetStatistics() {
-
-      // Set up
-      final int keyCount = MAX_SIZE;
-      final Map<String, String> map = new HashMap<String, String>(3);
-      final Cache<String, String> cache = cacheList.get(0);
-      for (int i = 0; i < keyCount; i++) {
-         map.put(createKey(i), createValue(i));
-      }
-
-      // Add
-      cache.putAll(map);
-
-      // Get statistics immediately after write and assert
-      final CacheStatistics statistics = cache.getStatistics();
-
-      // Read stats
-      assertEquals(0, statistics.getElementsOnDiskCount());
-      assertEquals(0, statistics.getReadHitCount());
-      assertEquals(0, statistics.getReadMissCount());
-      assertEquals(0.0f, statistics.getReadHitRatio());
-      assertEquals(0.0f, statistics.getReadMissRatio());
-
-      // Write stats
-      assertEquals(0, statistics.getWriteHitCount());
-      assertEquals(MAX_SIZE, statistics.getWriteMissCount());
-      assertEquals(0.0f, statistics.getWriteHitRatio());
-      assertEquals(1.0f, statistics.getWriteMissRatio());
-
-      // Do read
-      final int readSize = MAX_SIZE / 100;
-      final Set<String> readSet = new HashSet<String>(readSize);
-      for (int i = 0; i < readSize; i++) {
-         readSet.add(createKey(i));
-
-      }
-      cache.getAll(readSet);
-
-      final CacheStatistics readStats = cache.getStatistics();
-
-      // Read stats
-      assertEquals(0, readStats.getElementsOnDiskCount());
-      assertEquals(0, readStats.getReadMissCount());
-      assertEquals(1f, readStats.getReadHitRatio());
-      assertEquals(0f, readStats.getReadMissRatio());
-
-      // Write stats
-      assertEquals(0, readStats.getWriteHitCount());
-      assertEquals(MAX_SIZE, readStats.getWriteMissCount());
-      assertEquals(0.0f, readStats.getWriteHitRatio());
-      assertEquals(1.0f, readStats.getWriteMissRatio());
-
-
-      // Force eviction
-      for (int i = MAX_SIZE; i < MAX_SIZE + MAX_SIZE * 0.1; i++) {
-         map.put(createKey(i), createValue(i));
-      }
    }
 
 
