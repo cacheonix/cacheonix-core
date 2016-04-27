72f470d960f386376b775406ac8ff17e561e8b6f
Slava Imeshev <imeshev@yahoo.com>
2016-04-26 18:50:28 -0700

    Bumped protocol

--- src/org/cacheonix/impl/net/Protocol.java
+++ src/org/cacheonix/impl/net/Protocol.java
@@ -31,7 +31,7 @@ public final class Protocol {
    /**
     * Protocol version.
     */
-   public static final int PROTOCOL_VERSION = (byte) 11;
+   public static final int PROTOCOL_VERSION = (byte) 12;
 
 
    private Protocol() {
