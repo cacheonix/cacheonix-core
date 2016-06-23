b998b9d3c7fffecbf09887ba105b5a7da8736a07
Slava Imeshev <imeshev@yahoo.com>
2016-06-23 15:38:05 -0700

    Implemented CACHEONIX-475 - Add cache provider for PrimeFaces

---
+++ src/org/cacheonix/plugin/primefaces/PrimefacesCacheProvider.java
@@ -0,0 +1,8 @@
+package org.cacheonix.plugin.primefaces;
+
+/**
+ * Created by vimeshev on 6/23/16.
+ */
+public class PrimefacesCacheProvider {
+
+}
---
+++ test/src/org/cacheonix/plugin/primefaces/PrimefacesCacheProviderTest.java
@@ -0,0 +1,28 @@
+package org.cacheonix.plugin.primefaces;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by vimeshev on 6/23/16.
+ */
+public class PrimefacesCacheProviderTest extends TestCase {
+
+   public void testGet() throws Exception {
+
+   }
+
+
+   public void testPut() throws Exception {
+
+   }
+
+
+   public void testRemove() throws Exception {
+
+   }
+
+
+   public void testClear() throws Exception {
+
+   }
+}
