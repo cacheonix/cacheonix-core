8b1680fead80067734c53819419c02294771de8f
Slava Imeshev <imeshev@yahoo.com>
2016-07-17 15:51:18 -0700

    Added missing license header.

--- src/org/cacheonix/impl/BucketEventDispatcher.java
+++ src/org/cacheonix/impl/BucketEventDispatcher.java
@@ -1,3 +1,16 @@
+/*
+ * Cacheonix Systems licenses this file to You under the LGPL 2.1
+ * (the "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.cacheonix.org/products/cacheonix/license-lgpl-2.1.htm
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.cacheonix.impl;
 
 import org.cacheonix.impl.cache.distributed.partitioned.AssignBucketCommand;
