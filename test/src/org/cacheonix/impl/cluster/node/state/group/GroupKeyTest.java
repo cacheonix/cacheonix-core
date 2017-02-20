fdd197aa689ce2cda6d41adf649857181618aeb9
Slava Imeshev <imeshev@yahoo.com>
2017-02-20 13:32:18 -0800

    Added a test for GroupKey

---
+++ test/src/org/cacheonix/impl/cluster/node/state/group/GroupKeyTest.java
@@ -0,0 +1,28 @@
+package org.cacheonix.impl.cluster.node.state.group;
+
+import junit.framework.TestCase;
+
+/**
+ * A tester for {@link GroupKey}.
+ */
+public class GroupKeyTest extends TestCase {
+
+   public void testEquals() throws Exception {
+
+      assertEquals(new GroupKey(1, "test"), new GroupKey(1, "test"));
+      assertFalse(new GroupKey(1, "test").equals(new GroupKey(2, "test")));
+      assertFalse(new GroupKey(1, "test").equals(new GroupKey(1, "test2")));
+   }
+
+
+   public void testToString() throws Exception {
+
+      assertNotNull(new GroupKey(1, "test").toString());
+   }
+
+
+   public void testHashCode() throws Exception {
+
+      assertEquals(3556527, new GroupKey(1, "test").hashCode());
+   }
+}
